62eddc893f1f8b4a8ea890ffe5aa092c603e2762
Kisara Sandes <kisarasandes22@gmail.com>
2025-02-18 00:11:30 +0530

    Why Trust Us Section Done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import HeroSection from '../components/HeroSection/HeroSection';
+import WhyTrustUsSection from '../components/WhyTrustUs/WhyTrustUs';
 
 const Home = () => {
   return (
     <div>
+
       <Header />
       <HeroSection />
+      <WhyTrustUsSection />
+
     </div>
   );
 };
